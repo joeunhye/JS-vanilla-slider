51ed9d221d11f84b664b3871e5d12a9d77ec3f89
eunhye919 <eunhye919@vtex.co.kr>
2022-08-08 10:44:06 +0900

    슬라이드 페이지네이션 구현

--- type2/slider.js
+++ type2/slider.js
@@ -14,35 +14,73 @@ const slideItems = document.querySelectorAll(".slide_item");
 const maxSlide = slideItems.length;
 
 //현재 슬라이드 위치
-let currentSlide = 1;
+let currentSlide = 0;
+
+//페이지네이션 생성
+const pageNav = document.querySelector('.slide_pagination');
+
+for(let i = 0; i < maxSlide; i++) {
+	if(i === 0) {
+		// 첫번째 페이지네이션에 active 클래스 추가
+		pageNav.innerHTML += `<li class="active">*</li>` 
+	}else {
+		pageNav.innerHTML += `<li>*</li>`
+	}
+}
+
+const pageNavItems = document.querySelectorAll('.slide_pagination > li');
 
 //버튼 엘리먼트에 클릭 이벤트 추가
 nextBtn.addEventListener("click", () => {
 	currentSlide++;
-	if (currentSlide <= maxSlide) {
+	if (currentSlide < maxSlide) {
 		//슬라이드 이동을 위한 offset 계산
-		const offset = slideWidth * (currentSlide - 1);
+		const offset = slideWidth * currentSlide;
 		//각 슬라이드 아이템의 left에 offset 적용
 		slideItems.forEach(item => {
 			item.setAttribute("style", `left: ${-offset}px`);
 		});
+		// 슬라이드 이동 시 현재 활성화된 pagination 변경
+		pageNavItems.forEach(item => item.classList.remove('active'));
+		pageNavItems[currentSlide].classList.add('active');
 	} else {
 		currentSlide--;
 	}
 });
+
 prevBtn.addEventListener("click", () => {
 	currentSlide--;
-	if (currentSlide > 0) {
+	if (currentSlide >= 0) {
 		//슬라이드 이동을 위한 offset 계산
-		const offset = slideWidth * (currentSlide - 1);
+		const offset = slideWidth * currentSlide;
 		//각 슬라이드 아이템의 left에 offset 적용
 		slideItems.forEach(item => {
 			item.setAttribute("style", `left: ${-offset}px`);
 		});
+		// 슬라이드 이동 시 현재 활성화된 pagination 변경
+		pageNavItems.forEach(item => item.classList.remove('active'));
+		pageNavItems[currentSlide].classList.add('active');
 	} else {
 		currentSlide++;
 	}
 });
+
+//페이지네이션 클릭 시 해당 슬라이드 이동
+for(let i = 1; i < maxSlide; i++) {
+	pageNavItems[i].addEventListener('click', () => {
+		//클릭한 페이지네이션에 따라 현재 슬라이드 변경해주기
+		currentSlide = i;
+		//슬라이드 이동을 위한 offset 계산
+		const offset = slideWidth * currentSlide;
+		//각 슬라이드 아이템의 left에 offset 적용
+		slideItems.forEach(item => {
+			item.setAttribute("style", `left: ${-offset}px`);
+		});
+		// 슬라이드 이동 시 현재 활성화된 pagination 변경
+		pageNavItems.forEach(item => item.classList.remove('active'));
+		pageNavItems[currentSlide].classList.add('active');
+	})
+}
 
 //브라우저 화면 조정 시 slideWidth 다시 계산
 window.addEventListener("resize", () => {
